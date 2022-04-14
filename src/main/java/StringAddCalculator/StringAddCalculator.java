f648c56896c1c87e111d5ba7b7cc703bec84fb08
moonstal1506 <moonstal1506@gmail.com>
2022-04-14 22:10:31 +0900

    feat: “//”와 “\n” 문자 사이에 커스텀 구분자를 지정

--- src/main/java/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/StringAddCalculator/StringAddCalculator.java
@@ -1,19 +1,34 @@
 package StringAddCalculator;
 
-public class StringAddCalculator {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class StringAddCalculator {
+    
     public static int splitAndSum(String text) {
         int result=0;
         if (text == null||text.isEmpty()) {
             return 0;
         }
         if(text.length()==1){
-            result = Integer.parseInt(text);
+            return Integer.parseInt(text);
+        }
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] numbers= m.group(2).split(customDelimiter);
+            return add(numbers);
         }
 
         String[] numbers = text.split(",|:");
-        for(String number:numbers){
-            result+=Integer.parseInt(number);
+        return add(numbers);
+    }
+
+    private static int add(String[] tokens){
+        int result = 0;
+        for(String token:tokens){
+            result+= Integer.parseInt(token);
         }
         return result;
     }
