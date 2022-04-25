67438b9a9a7c7cb59801345ea40f609f2195283d
moonstal1506 <moonstal1506@gmail.com>
2022-04-25 14:42:37 +0900

    feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,15 +1,38 @@
 package calculator;
 
 public class StringAddCalculator {
-    public static int splitAndSum(String number) {
-        if(isNullOrEmpty(number)){
+
+    public static int splitAndSum(String text) {
+        if(isNullOrEmpty(text)){
             return 0;
         }
-        return 0;
+
+        return add(toInts(splitString(text)));
+
     }
 
-    private static boolean isNullOrEmpty(String number){
-        return number==null||number.isEmpty();
+    private static boolean isNullOrEmpty(String text){
+        return text==null||text.isEmpty();
+    }
+
+    private static String[] splitString(String text){
+        return text.split(",");
+    }
+
+    private static int[] toInts(String[] texts){
+        int[] numbers=new int[texts.length];
+        for(int i=0;i<texts.length;i++){
+           numbers[i]= Integer.parseInt(texts[i]);
+        }
+        return numbers;
+    }
+
+    private static int add(int[] numbers){
+        int result=0;
+        for(int number: numbers ){
+            result+=number;
+        }
+        return result;
     }
 
 }
